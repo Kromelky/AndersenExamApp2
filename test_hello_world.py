8fb29e16513700cc85269497c164911033ed0d12
Артем Гусаров <kromelky@gmail.com>
2021-10-19 22:28:45 +0300

    Initial dev commit

--- test_hello_world.py
+++ test_hello_world.py
@@ -8,9 +8,11 @@ class TestHelloWorld(unittest.TestCase):
         self.app = main.app.test_client()
         self.app.testing = True
 
+
     def test_status_code(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
+
 
     def test_message(self):
         response = self.app.get('/')
