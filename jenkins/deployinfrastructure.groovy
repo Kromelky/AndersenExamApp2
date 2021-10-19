5871c81d00489221408e6b1130629b06c74d5527
Артем Гусаров <kromelky@gmail.com>
2021-10-19 22:47:20 +0300

    Initial dev commit

--- jenkins/deployinfrastructure.groovy
+++ jenkins/deployinfrastructure.groovy
@@ -6,7 +6,6 @@ pipeline {
         registryCredentials = "nexus-jenkins-acc"
         registry = "10.0.0.179:8085/"
         repo = "https://github.com/Kromelky/AndersenExamApp2"
-        imageName = 'kromelky/application2'
         gitHubAuthId = 'git-kromelky-token'
         nexus_login = "nexus-acc"
         application_label = "2"
--- terraform/Security.tf
+++ terraform/Security.tf
@@ -9,6 +9,7 @@ resource "aws_security_group" "sg_allow_ssh" {
       to_port          = 22
       protocol         = "tcp"
       cidr_blocks      = ["0.0.0.0/0"]
+
     }
   
 
