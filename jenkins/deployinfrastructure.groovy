908f4ef28b2cf2a56a9d9bcc4e2e9c8f6fcd6726
Артем Гусаров <kromelky@gmail.com>
2021-10-19 23:01:08 +0300

    Initial dev commit

--- jenkins/deployinfrastructure.groovy
+++ jenkins/deployinfrastructure.groovy
@@ -6,7 +6,7 @@ pipeline {
         registryCredentials = "nexus-jenkins-acc"
         registry = "10.0.0.179:8085/"
         repo = "https://github.com/Kromelky/AndersenExamApp2"
-        imageName = 'kromelky/application2'
+        locaimageName = 'kromelky/application2'
         gitHubAuthId = 'git-kromelky-token'
         nexus_login = "nexus-acc"
         application_label = "2"
@@ -40,7 +40,7 @@ pipeline {
                 dir("terraform"){
                     withCredentials([usernamePassword(credentialsId: registryCredentials, passwordVariable: 'C_PASS', usernameVariable: 'C_USER')]) {
                         sh """
-                         terraform apply -var-file="tfvars/dev.tfvars" -var "docker_pass=${C_PASS}" -var "docker_login=${C_USER}" -var "instance_label=${application_label}" -var "imageName=${imageName}" -auto-approve
+                         terraform apply -var-file="tfvars/dev.tfvars" -var "docker_pass=${C_PASS}" -var "docker_login=${C_USER}" -var "instance_label=${application_label}" -var "imageName=${locaimageName}" -auto-approve
                          """
                     }
                 }
