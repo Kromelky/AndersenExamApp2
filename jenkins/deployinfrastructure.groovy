b0a248d696a7701492949f0365f3b8c5adc0df57
Артем Гусаров <kromelky@gmail.com>
2021-10-20 22:31:08 +0300

    Initial dev commit

--- jenkins/deployinfrastructure.groovy
+++ jenkins/deployinfrastructure.groovy
@@ -16,7 +16,7 @@ pipeline {
     stages {
         stage('Init terraform') {
             steps {
-                dir("terraform"){
+                dir("terraform/dev"){
                     sh "terraform init"
                 }
             }
@@ -25,7 +25,7 @@ pipeline {
 
         stage('Plan terraform') {
             steps {
-                dir("terraform"){
+                dir("terraform/dev"){
                     withCredentials([usernamePassword(credentialsId: registryCredentials, passwordVariable: 'C_PASS', usernameVariable: 'C_USER')]) {
                         sh """
                         terraform plan -var-file="tfvars/dev.tfvars" -var "docker_pass=${C_PASS}" -var "docker_login=${C_USER}" -var "imageName=${imageName}"
@@ -37,7 +37,7 @@ pipeline {
 
         stage('Apply terraform') {
             steps {
-                dir("terraform"){
+                dir("terraform/dev"){
                     withCredentials([usernamePassword(credentialsId: registryCredentials, passwordVariable: 'C_PASS', usernameVariable: 'C_USER')]) {
                         sh """
                          terraform apply -var-file="tfvars/dev.tfvars" -var "docker_pass=${C_PASS}" -var "docker_login=${C_USER}" -var "imageName=${imageName}" -auto-approve
