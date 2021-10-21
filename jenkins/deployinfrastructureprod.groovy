93d9cd49bb881c4b5728dcdff98548c833c94c5a
Gusarov <gusarov@p310.rniirs.ru>
2021-10-21 17:11:24 +0300

    Change Deployment script

--- jenkins/deployinfrastructureprod.groovy
+++ jenkins/deployinfrastructureprod.groovy
@@ -34,19 +34,9 @@ pipeline {
                                 """
                             }
                             catch (Exception ex) {
-                                println(ex.getMessage())
-                                if (ex.getMessage().contains("-migrate-state")){
                                     sh """
                                     terraform init -migrate-state
                                     terraform plan -var-file="tfvars/prod.tfvars" -var "docker_pass=${C_PASS}" -var "docker_login=${C_USER}" -var "imageName=${imageName}" -var "instance_label=${application_label}"
-                                    """
-                                }
-                                else {
-                                    sh """
-                                    terraform init -migrate-state
-                                    terraform plan -var-file="tfvars/prod.tfvars" -var "docker_pass=${C_PASS}" -var "docker_login=${C_USER}" -var "imageName=${imageName}" -var "instance_label=${application_label}"
-                                    """
-                                }
                             }
                         }
                     }
