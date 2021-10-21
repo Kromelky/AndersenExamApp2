57a76c320bfd986c8a5a7809f3c2cb024e7e61e9
Gusarov <gusarov@p310.rniirs.ru>
2021-10-21 16:02:32 +0300

    Change Deployment script

--- jenkins/deployinfrastructureprod.groovy
+++ jenkins/deployinfrastructureprod.groovy
@@ -27,9 +27,21 @@ pipeline {
             steps {
                 dir("terraform/prod"){
                     withCredentials([usernamePassword(credentialsId: registryCredentials, passwordVariable: 'C_PASS', usernameVariable: 'C_USER')]) {
+                        try {
+                            sh """
+                            terraform plan -var-file="tfvars/prod.tfvars" -var "docker_pass=${C_PASS}" -var "docker_login=${C_USER}" -var "imageName=${imageName}" -var "instance_label=${application_label}"
+                            """
+                        }
+                        catch (Exception ex)
+                        {
+                            if ex.getMessage().contains("-migrate-state")
+                        }
                         sh """
-                        terraform plan -var-file="tfvars/prod.tfvars" -var "docker_pass=${C_PASS}" -var "docker_login=${C_USER}" -var "imageName=${imageName}" -var "instance_label=${application_label}"
-                        """
+                            terraform init -migrate-state
+                           """
+                        sh """
+                            terraform plan -var-file="tfvars/prod.tfvars" -var "docker_pass=${C_PASS}" -var "docker_login=${C_USER}" -var "imageName=${imageName}" -var "instance_label=${application_label}"
+                         """
                     }
                 }
             }
