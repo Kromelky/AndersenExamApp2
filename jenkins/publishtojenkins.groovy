afe52a8849290a6a4f992cde4c5a45eddc01ee32
Артем Гусаров <kromelky@gmail.com>
2021-10-22 11:12:01 +0300

    Test

--- jenkins/publishtojenkins.groovy
+++ jenkins/publishtojenkins.groovy
@@ -98,7 +98,7 @@ def notifyStarted() {
 
     emailext (
             subject: "STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
-            body: """<p>STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+            body: """<p>STARTED: Job2 '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
         <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>""",
             recipientProviders: [[$class: 'DevelopersRecipientProvider']]
     )
