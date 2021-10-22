69cec14fb6c50f3a117f70ca666ac4be44f3070e
Артем Гусаров <kromelky@gmail.com>
2021-10-22 14:17:09 +0300

    Commented python linter

--- jenkins/publishtojenkins.groovy
+++ jenkins/publishtojenkins.groovy
@@ -29,13 +29,13 @@ pipeline {
 
         // Linter Check
 
-        stage('Linter check') {
+       /*  stage('Linter check') {
             steps {
                 sh 'python3 -m pylint --output-format=parseable --fail-under=7.0 main.py --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" | tee pylint.log || echo "pylint exited with $?"'
                 echo "linting Success, Generating Report"
                 recordIssues enabledForFailure: true, aggregatingResults: true, tool: pyLint(pattern: 'pylint.log')
             }
-        }
+        } */
 
 
         // add maven build
