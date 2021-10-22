13b8c82fb680085288662134c574ee0b113500b7
Артем Гусаров <kromelky@gmail.com>
2021-10-22 13:40:21 +0300

    Added python linter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from flask import Flask, request, jsonify
 import argparse
-
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 app.config.update(
@@ -10,32 +9,32 @@ app.config.update(
 )
 
 
-def get_AuthorMessage(ser_name, name):
+def get_authormessage(ser_name, name):
     return f"Made with {ser_name} by {name}"
 
 
-def processRequest(req):
+def processrequest():
     response = jsonify(id=1, message="HelloWorld from Python")
     return response
 
 
 @app.route("/", methods=['GET', 'POST'])
-def httpRoot():
-    return processRequest(request)
+def httproot():
+    return processrequest()
 
 
-def runHttp():
+def runhttp():
     print("Running http")
     app.run(host=args.host, port=args.port, threaded=True, debug=False)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog="AwesomeZoo Flask Application")
-    parser.add_argument('-s', '--source', help=' Change emogipedia data source. ', type=str, default='https://emojipedia.org/nature/')
-    parser.add_argument('-H', '--host', help=' Change application host ', type=str, default='0.0.0.0')
+    parser.add_argument('-s', '--source', help=' Change emogipedia data source. ',
+                        type=str, default='https://emojipedia.org/nature/')
+    parser.add_argument('-H', '--host', help=' Change application host ',
+                        type=str, default='0.0.0.0')
     parser.add_argument('-p', '--port', help=' Change listening post', type=int,
                         default=8080)
     args = parser.parse_args()
-    runHttp()
-
-
+    runhttp()
